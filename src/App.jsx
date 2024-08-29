327dfead4d86a37c90bb2575df25b709cb1b54c9
killflex <ferryhasanbackup@gmail.com>
2024-08-29 22:01:18 +0700

    feat: useState btn with null

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import { useState } from "react";
 import ButtonTic from "./components/ButtonTic";
 
 function App() {
+  const [value, setValue] = useState(Array(9).fill());
+
+  const valueX = (i) => {
+    const nextSquares = value.slice();
+    nextSquares[i] = "X";
+
+    setValue(nextSquares);
+  };
+
   return (
     <>
       <div className="flex flex-wrap w-[105px] h-[105px]">
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
-        <ButtonTic />
+        <ButtonTic btnText={value[0]} btnEvent={() => valueX(0)} />
+        <ButtonTic btnText={value[1]} btnEvent={() => valueX(1)} />
+        <ButtonTic btnText={value[2]} btnEvent={() => valueX(2)} />
+        <ButtonTic btnText={value[3]} btnEvent={() => valueX(3)} />
+        <ButtonTic btnText={value[4]} btnEvent={() => valueX(4)} />
+        <ButtonTic btnText={value[5]} btnEvent={() => valueX(5)} />
+        <ButtonTic btnText={value[6]} btnEvent={() => valueX(6)} />
+        <ButtonTic btnText={value[7]} btnEvent={() => valueX(7)} />
+        <ButtonTic btnText={value[8]} btnEvent={() => valueX(8)} />
       </div>
     </>
   );
--- src/components/ButtonTic.jsx
+++ src/components/ButtonTic.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { useState } from "react";
 
 const ButtonTic = (props) => {
-  const [value, setValue] = useState("");
-
-  function valueX() {
-    setValue("X");
-  }
-
   return (
     <>
       <button
         className="bg-white text-black h-[35px] w-[35px] border border-black font-bold text-xl cursor-pointer"
-        onClick={valueX}
+        onClick={props.btnEvent}
       >
-        {value}
+        {props.btnText}
       </button>
     </>
   );
